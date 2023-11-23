b8cb93b49cf06373672ed313784fa0bebf7cf157
heyqule <hq.jfax@gmail.com>
2023-11-22 22:04:13 -0500

    Check once every 6 hours

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-    sleep 24*60*60 &  # We sleep in the background to make the script interruptable via SIGTERM when running in docker
+    sleep 21600 &  # We sleep in the background to make the script interruptable via SIGTERM when running in docker
     wait $!
     python main.py
 done
