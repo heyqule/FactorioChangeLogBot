6cc7386a6653f13940624a8cf7235fa0873ae2cf
heyqule <hq.jfax@gmail.com>
2023-11-26 17:17:58 -0500

    Idle time changed to 2hrs instead of 6hrs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-    sleep 21600 &  # We sleep in the background to make the script interruptable via SIGTERM when running in docker
+    sleep 7200 &  # We sleep in the background to make the script interruptable via SIGTERM when running in docker
     wait $!
     python main.py
 done
